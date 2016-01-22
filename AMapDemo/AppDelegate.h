f81300ec8a3f8c210291ebe6e8afa580fce0e4a6
yrq110 <yrq1025@gmail.com>
2016-01-22 16:10:45 +0800

    Initial Commit

---
+++ AMapDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AMapDemo
+//
+//  Created by yrq_mac on 16/1/22.
+//  Copyright © 2016年 yrq_mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AMapDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AMapDemo
+//
+//  Created by yrq_mac on 16/1/22.
+//  Copyright © 2016年 yrq_mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
